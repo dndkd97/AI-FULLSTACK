6eccd9563b530f9170cb0fe995aad37a32501b7a
dndkd97 <dndkd97@naver.com>
2026-04-01 09:14:30 +0900

    day004

--- track003_java/bankproject/src/bankproject/bank.java
+++ track003_java/bankproject/src/bankproject/bank.java
@@ -5,7 +5,7 @@ public class bank {
 	public static void main(String[] args) {
 		
 		System.out.println("WELCOME! (주)CODE BANK");
-		System.out.print("* 1.추가 /n "+ "*2.조회");
+		System.out.print("* 1.추가 "+ "*2.조회");
 		
 		
 		
