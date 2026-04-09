25249a1679e18bba31e24e06a10704be3c03c050
dndkd97 <dndkd97@naver.com>
2026-04-09 18:15:06 +0900

    java

---
+++ track003_java/java2026/src/com/the703/basic007/For2Ex005.java
@@ -0,0 +1,15 @@
+package com.the703.basic007;
+
+public class For2Ex005 {
+
+	public static void main(String[] args) {
+		
+		
+		    for(int i=1; i<=1; i++) {
+			for(int a=1; a<=4; a++) {System.out.println("1234");}
+			System.out.println();}
+		
+
+	}
+
+}
