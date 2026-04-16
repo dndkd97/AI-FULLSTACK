27bd95604e38275bb4a24032385ca424119dc35b
dndkd97 <dndkd97@naver.com>
2026-04-16 12:55:32 +0900

    day012

---
+++ track003_java/java2026/src/com/the703/basic009_ex/MethodEx001.java
@@ -0,0 +1,26 @@
+package com.the703.basic009_ex;
+
+public class MethodEx001 {
+
+	public static void dog() {
+		System.out.println("멍멍");
+	}
+	
+	public static void cat() {
+		System.out.println("냐옹");
+	}
+	
+	public static void line() {
+		System.out.println("========");
+	}
+
+	public static void main(String[] args) {
+		
+		dog();
+		line();
+		cat();
+		line();
+
+	}
+
+}
