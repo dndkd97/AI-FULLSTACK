351932c4e975ff1d53c4221743d9c5576ee575e3
dndkd97 <dndkd97@naver.com>
2026-04-16 14:43:22 +0900

    day015

--- track003_java/java2026/src/com/the703/basic009_ex/MethodEx002.java
+++ track003_java/java2026/src/com/the703/basic009_ex/MethodEx002.java
@@ -4,16 +4,15 @@ public class MethodEx002 {
 	
 	
 	public static void test1(int num) {System.out.println(num);}
+	
 	public static void test2(double num) {System.out.println(num);}
+	
 	public static void hap(int num, int num1 ) 
 	{int sum=0;  for(int i=num; i<=num1; i++) 
 	{ sum+=i; System.out.print(i+(i==num1?"=":"+"));}System.out.println(sum);}
 	
-		
-	public static void disp(int num, char ch) {	for(int i=1; i<=num; i++) {System.out.print("*");}}
+	public static void disp(int num, char ch) {	for(int i=1; i<=num; i++) {System.out.print(ch);}}
 	
-	
-
 	public static void main(String[] args) {
 		  // public static  리턴값 메서드명(파라미터)
 	     test1(10);    //10 출력
