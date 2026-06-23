5bf906a8e9e722ba2ace68dbfa5f844f4c0b02d6
dndkd97 <dndkd97@naver.com>
2026-06-23 18:19:19 +0900

    day059

---
+++ track004_2_oracle/001.oracle.sql
@@ -0,0 +1,30 @@
+--1. oracle
+--- 데이터베이스 언어
+--1) 데이터 정의어(DDL) : Create , Alter , Drop (cad)
+--2) 데이터 조작어(DML) : insert , select , update , delete (crud)
+--3) 데이터 제어어(DCL) : grant , revoke
+
+- 1. oracle 설치
+- 2. sql developer 설치 (sql 편집)
+- 3. 사용
+<실습1>
+```sql(cmd)
+sqlplus
+conn system/1234
+
+-- 유저만들기(오라클 12 이상에서 기존방식으로 사용자 생성 허용 - c#scott / tiger )
+ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+create user scott identified by tiger;
+
+-- 권한부여
+grant connect , resource to scott;
+
+ALTER USER scott DEFAULT TABLESPACE users QUOTA UNLIMITED ON users; -- 물리적 공간 이용
+grant create table to scott;
+```
+<실습2>
+```sql(sqldeveloper)
+
+```
+
+
