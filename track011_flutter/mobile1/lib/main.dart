240d098d7b95f0bc75b35dab4e571e07dec3b7ea
dndkd97 <dndkd97@naver.com>
2026-09-16 15:15:27 +0900

    flutter

---
+++ track011_flutter/mobile1/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+// 1. 구동시작점
+void main() {
+    runApp(
+      MaterialApp(
+        home: Scaffold(
+          appBar:AppBar(title:Text('내 첫 Flutter 앱 703')),
+          body:Center(child: Text('Hello Flutter!-하이 홍길동')),
+        ),
+      ),
+    );
+}
+// Scaffold : 전체 기본뼈대 - 상단바, 본문 제공하는 기본 위젯
+// appBar : 상단 타이틀 바
+// body : 몸통구성
